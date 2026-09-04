a447ad1e3fd65e131b51d225d85f381677e8cf5c
ThoutiReddyGanesh <ganeshthoutireddy@gmail.com>
2026-09-04 12:23:14 +0530

    Time: 0 ms (100%), Space: 43.6 MB (82.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,21 +3,54 @@ class Solution {
         int low=0;
         int high=nums.length-1;
 
+        //FIND PIVOT
+        while(low<high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]>nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+
+        int pivot=low;
+
+        //SEARCH LEFT HALF
+        low=0;
+        high=pivot-1;
+
         while(low<=high){
-        int mid=low+(high-low)/2;
+            int mid=low+(high-low)/2;
 
-        if(nums[mid]==target) return mid;
+            if(nums[mid]==target)return mid;
 
-        if(nums[low]<=nums[mid]){
-            if(nums[low]<=target && target<nums[mid]) high=mid-1;
-            else low=mid+1;
-        }   
-        
-        else{
-            if(nums[mid]<target &&  target<=nums[high]) low=mid+1;
-            else high=mid-1;
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
+
+        //SEARCH RIGHT HALF
+        low=pivot;
+        high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target)return mid;
+
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
+
         return -1;
     }
 }
