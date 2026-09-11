abfecfaecb0ef098a7a43831507b6650694b6d99
ThoutiReddyGanesh <ganeshthoutireddy@gmail.com>
2026-09-11 21:10:58 +0530

    Time: 1 ms (97.5%), Space: 43.2 MB (81.55%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isCircularSentence(String s) {
+
+        String[] words=s.split(" ");
+        if(words.length==1)
+            return words[0].charAt(0)==words[0].charAt(words[0].length()-1);
+
+        for(int i=0;i<words.length-1;i++){
+            if(words[i].charAt(words[i].length()-1)!=words[i+1].charAt(0))
+                return false;
+        }
+
+        if(words[words.length-1].charAt(words[words.length-1].length()-1)!=words[0].charAt(0))
+            return false;
+
+        return true;
+    }
+}
