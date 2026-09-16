58e52eb9d7cc456f8ada15425f44fb1bf4369f4e
ThoutiReddyGanesh <ganeshthoutireddy@gmail.com>
2026-09-16 19:23:40 +0530

    Time: 10 ms (1.72%), Space: 47.3 MB (6.71%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,61 +1,54 @@
-import java.util.ArrayList;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.*;
 import java.math.BigInteger;
-
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // Step 1: Extract digits from l1 into ArrayList
-        ListNode temp = l1;
-        ArrayList<Integer> al = new ArrayList<>();
-        while (temp != null) {
+class Solution{
+    public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+        ListNode temp=l1;
+        ArrayList<Integer> al=new ArrayList<>();
+        while(temp!=null){
             al.add(temp.val);
-            temp = temp.next;
-        }
+            temp=temp.next;}
 
-        // Step 2: Build BigInteger sum from l1 digits
-        BigInteger sum = BigInteger.ZERO;
-        for (int i = al.size() - 1; i >= 0; i--) {
-            sum = sum.multiply(BigInteger.TEN).add(BigInteger.valueOf(al.get(i)));
-        }
-        
-        // Step 3: Extract digits from l2 into ArrayList
-        ListNode temp2 = l2;
-        ArrayList<Integer> al2 = new ArrayList<>();
-        while (temp2 != null) {
+        BigInteger sum=BigInteger.ZERO;
+        for(int i=al.size()-1;i>=0;i--){
+            sum=sum.multiply(BigInteger.TEN).add(BigInteger.valueOf(al.get(i)));}
+
+        ListNode temp2=l2;
+        ArrayList<Integer> al2=new ArrayList<>();
+        while(temp2!=null){
             al2.add(temp2.val);
-            temp2 = temp2.next;
+            temp2=temp2.next;}
+
+        BigInteger sum2=BigInteger.ZERO;
+        for(int j=al2.size()-1;j>=0;j--){
+            sum2=sum2.multiply(BigInteger.TEN).add(BigInteger.valueOf(al2.get(j)));}
+
+        BigInteger add=sum.add(sum2);
+        ListNode nh=null;
+        ListNode t=null;
+        ArrayList<Integer> re=new ArrayList<>();
+        String s=add.toString();
+        for(int k=s.length()-1;k>=0;k--){
+            re.add(s.charAt(k)-'0');}
+
+        for(int x:re){
+            ListNode n=new ListNode(x);
+            if(nh==null){
+                nh=n;
+                t=n;}
+            else{
+                t.next=n;
+                t=n;}
         }
-
-        // Step 4: Build BigInteger sum2 from l2 digits
-        BigInteger sum2 = BigInteger.ZERO;
-        for (int j = al2.size() - 1; j >= 0; j--) {
-            sum2 = sum2.multiply(BigInteger.TEN).add(BigInteger.valueOf(al2.get(j)));
-        }
-        
-        // Step 5: Add both BigIntegers
-        BigInteger add = sum.add(sum2);
-
-        // Step 6: Convert sum string back into a reverse-ordered linked list
-        ListNode nh = null;
-        ListNode t = null;
-        ArrayList<Integer> re = new ArrayList<>();
-        String s = String.valueOf(add);
-
-        for (int k = s.length() - 1; k >= 0; k--) {
-            re.add(s.charAt(k) - '0');
-        }
-
-        for (int x : re) {
-            ListNode n = new ListNode(x);
-
-            if (nh == null) {
-                nh = n;
-                t = n;
-            } else {
-                t.next = n;
-                t = n;
-            }
-        }
-
         return nh;
     }
 }
