9766182ea45913b25fb69f3be2b4721fe087da16
ThoutiReddyGanesh <ganeshthoutireddy@gmail.com>
2026-09-19 18:06:09 +0530

    Time: 6 ms (47.51%), Space: 43.2 MB (23.39%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.java
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.java
@@ -5,16 +5,12 @@ class Solution{
         int c=3*n;
         String result=""+a+b+c;
         int count=0;
-
         for(int i=1;i<=9;i++){
             if(result.indexOf(String.valueOf(i))!=-1){
-                count++;
-            }
+                count++;}
         }
-
         if(count==9 && result.length()==9)
             return true;
-
         return false;
     }
 }
